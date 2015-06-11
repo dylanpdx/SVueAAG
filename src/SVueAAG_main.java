b008c6d7405d5e6de84581f822ccbbd5cad00583
Dylan Grinberg <dylanpdx@gmail.com>
2015-06-10 19:51:43 -0700

    Added a check if logged in correctly
    I kept typing my username wrong :frowning:

--- src/SVueAAG_main.java
+++ src/SVueAAG_main.java
@@ -14,22 +14,40 @@ public class SVueAAG_main {
 		Scanner scan = new Scanner(System.in);
 		String user = ""; // FOR DEBUGGING ONLY
 		String pass = ""; // FOR DEBUGGING ONLY
+		Boolean login = false;
 		
-		user = scan.next(); // FOR DEBUGGING ONLY
-		pass = scan.next(); // FOR DEBUGGING ONLY
+		Document document = new Document("");
+		Connection.Response loginForm = null; // Lazy fix?
 		
-        Connection.Response loginForm = Jsoup.connect("https://parentvue.beaverton.k12.or.us/Login_Student_PXP.aspx")
-                .method(Connection.Method.GET)
-                .execute();
 		
-		Document document = Jsoup.connect("https://parentvue.beaverton.k12.or.us/Login_Student_PXP.aspx")
-				.data("username",user) // FOR DEBUGGING ONLY
-				.data("password",pass) // FOR DEBUGGING ONLY
-				.data("__VIEWSTATE","/wEPDwUKMTk4OTU4MDc2NWRkpO1Yx46hRy/mkPaHIpJ41xe4NWOOH0l6Fqt2KzXrH58=")
-				.data("__VIEWSTATEGENERATOR","C520BE40")
-				.data("__EVENTVALIDATION","/wEdAASxlVz5jDGwq0WlapthbhhRKhoCyVdJtLIis5AgYZ/RYe4sciJO3Hoc68xTFtZGQEgSYOQVAPr9tiF9q7nSHjzop2Rg4jlbiMUFEEk7o+ulgkFZg4aqMI964lA977RcK24=")
-				.cookies(loginForm.cookies())
-				.post();
+		while (!login){
+			user = scan.next(); // FOR DEBUGGING ONLY
+			pass = scan.next(); // FOR DEBUGGING ONLY
+			
+			
+	        loginForm = Jsoup.connect("https://parentvue.beaverton.k12.or.us/Login_Student_PXP.aspx")
+	                .method(Connection.Method.GET)
+	                .execute();
+			
+			document = Jsoup.connect("https://parentvue.beaverton.k12.or.us/Login_Student_PXP.aspx")
+					.data("username",user) // FOR DEBUGGING ONLY
+					.data("password",pass) // FOR DEBUGGING ONLY
+					.data("__VIEWSTATE","/wEPDwUKMTk4OTU4MDc2NWRkpO1Yx46hRy/mkPaHIpJ41xe4NWOOH0l6Fqt2KzXrH58=")
+					.data("__VIEWSTATEGENERATOR","C520BE40")
+					.data("__EVENTVALIDATION","/wEdAASxlVz5jDGwq0WlapthbhhRKhoCyVdJtLIis5AgYZ/RYe4sciJO3Hoc68xTFtZGQEgSYOQVAPr9tiF9q7nSHjzop2Rg4jlbiMUFEEk7o+ulgkFZg4aqMI964lA977RcK24=")
+					.cookies(loginForm.cookies())
+					.post();
+			//System.out.println(document);
+			if (document.toString().contains("Invalid user id or password")){
+				System.out.println("Invalid user id or password.");
+			}else{
+				login = true;
+			}
+			
+		}
+
+		
+
 		
 		//System.out.println(document);
 		String welcome = document.getElementsByClass("UserHead").text();
@@ -45,7 +63,11 @@ public class SVueAAG_main {
 		
 		Element table = grades.select("table[class=info_tbl]").first();
 		table = table.select("tbody").get(0);
-		System.out.println(table.select("tr").size() - 1);
+		Elements gradeTBL = table.select("tr");
+		
+		
+		
+		
 		
 	}
 
